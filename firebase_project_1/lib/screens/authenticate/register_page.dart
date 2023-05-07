52a4bed575aa0ff68739c7e169f5dfdde2bbf1fb
Mahesh1772 <87927591+Mahesh1772@users.noreply.github.com>
2023-05-07 10:56:09 +0800

    Adding user details to Firesbase cloud

--- firebase_project_1/lib/screens/authenticate/register_page.dart
+++ firebase_project_1/lib/screens/authenticate/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -15,14 +16,42 @@ class _RegisterPageState extends State<RegisterPage> {
   final _emailId = TextEditingController();
   final _password = TextEditingController();
   final _confirmedpassword = TextEditingController();
+  final _rank = TextEditingController();
+  final _name = TextEditingController();
+  final _appointment = TextEditingController();
+  final _rationType = TextEditingController();
+  final _status = TextEditingController();
+  final _mobileNumber = TextEditingController();
+  final _bloodgroup = TextEditingController();
+  final _dob = TextEditingController();
+  final _ord = TextEditingController();
 
   Future signUp() async {
     if (_confirmedpassword.text.trim() == _password.text.trim()) {
+      //creating user
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: _emailId.text.trim(),
         password: _password.text.trim(),
       );
+
+      //Adding user details
+      addUserDetails();
     }
+  }
+
+  Future addUserDetails() async {
+    await FirebaseFirestore.instance.collection('Users').add({
+      //User map formatting
+      'rank': _rank.text.trim(),
+      'name': _name.text.trim(),
+      'appointment': _appointment.text.trim(),
+      'rationType': _rationType.text.trim(),
+      'status': _status.text.trim(),
+      'mobileNumber': _mobileNumber.text.trim(),
+      'bloodgroup': _bloodgroup.text.trim(),
+      'dob': _dob.text.trim(),
+      'ord': _ord.text.trim(),
+    });
   }
 
   @override
@@ -30,6 +59,15 @@ class _RegisterPageState extends State<RegisterPage> {
     _emailId.dispose();
     _password.dispose();
     _confirmedpassword.dispose();
+    _rank.dispose();
+    _name.dispose();
+    _appointment.dispose();
+    _rationType.dispose();
+    _status.dispose();
+    _mobileNumber.dispose();
+    _bloodgroup.dispose();
+    _dob.dispose();
+    _ord.dispose();
     super.dispose();
   }
 
@@ -45,33 +83,227 @@ class _RegisterPageState extends State<RegisterPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const SizedBox(
-                  height: 120,
+                  height: 20,
                 ),
-                const Icon(
-                  Icons.military_tech_outlined,
-                  size: 175,
-                  color: Colors.deepPurpleAccent,
-                ),
-                const SizedBox(height: 80),
+
                 //welcome text
                 Text(
                   'Congrats on Graduation!',
                   style: GoogleFonts.kanit(
                     fontSize: 30,
-                    fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.w600,
                     color: Colors.purple.shade300,
                   ),
                 ),
-                const SizedBox(height: 10),
+                const SizedBox(height: 5),
                 Text(
                   'Make your life easier, Register',
                   style: GoogleFonts.kanit(
                     fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.w500,
                     color: Colors.purple.shade400,
                   ),
                 ),
-                const SizedBox(height: 50),
+                const SizedBox(height: 30),
+
+                // rank
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _rank,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Enter your Rank'),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                // name
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _name,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Enter Name as in NRIC'),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                // Appointment
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _appointment,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Your Appointment'),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                // ration type
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _rationType,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Your Ration type'),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                // status
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _status,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Enter Any statuses'),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                // mobile number
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _mobileNumber,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Your Mobile Number'),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                // blood type
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _bloodgroup,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Your blood group'),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                // Date of Birth
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _dob,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Date of Birth'),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                // ORD date
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        controller: _ord,
+                        decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Your favourite ORD date'),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+
                 // email
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
@@ -93,6 +325,7 @@ class _RegisterPageState extends State<RegisterPage> {
                   ),
                 ),
                 const SizedBox(height: 10),
+
                 // password
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
@@ -115,6 +348,7 @@ class _RegisterPageState extends State<RegisterPage> {
                   ),
                 ),
                 const SizedBox(height: 10),
+
                 // confirm password
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
@@ -137,6 +371,7 @@ class _RegisterPageState extends State<RegisterPage> {
                   ),
                 ),
                 const SizedBox(height: 10),
+
                 // sign in
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
@@ -169,7 +404,7 @@ class _RegisterPageState extends State<RegisterPage> {
                     ),
                   ),
                 ),
-                const SizedBox(height: 35),
+                const SizedBox(height: 15),
 
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
