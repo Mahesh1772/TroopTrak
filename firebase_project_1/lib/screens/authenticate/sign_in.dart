5cfef1ecf020cb6bfb9be2f6c31520af693861d9
Mahesh1772 <87927591+Mahesh1772@users.noreply.github.com>
2023-05-28 17:18:34 +0800

    Sign in page validation

--- firebase_project_1/lib/screens/authenticate/sign_in.dart
+++ firebase_project_1/lib/screens/authenticate/sign_in.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_project_1/screens/authenticate/forgot_password_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:animated_snack_bar/animated_snack_bar.dart';
 
 class SignIn extends StatefulWidget {
   final VoidCallback showRegisterPage;
@@ -15,13 +17,47 @@ class _SignInState extends State<SignIn> {
   //Placeholders for the email and password input by user
   final _emailId = TextEditingController();
   final _password = TextEditingController();
+  var error = '';
 
   Future isSignedIn() async {
     //print('tap detected');
-    UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: _emailId.text.trim(),
-      password: _password.text.trim(),
-    );
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailId.text.trim(),
+        password: _password.text.trim(),
+      );
+    } on FirebaseAuthException catch (e) {
+      AnimatedSnackBar(
+        builder: ((context) {
+          return SafeArea(
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              color: Colors.redAccent,
+              height: 50,
+              width: 1000,
+              child: Expanded(
+                child: Row(
+                  children: const [
+                    Icon(Icons.error_outline_outlined),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Wrong Email or Password or Both',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }),
+      ).show(context);
+    }
   }
 
   @override
@@ -33,178 +69,244 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
+    String? validateEmail(String val) {
+      if (val.isEmpty) {
+        return "Email can not be empty";
+      } else if (!EmailValidator.validate(val, true)) {
+        return "Invalid Email Address";
+      }
+      return '';
+    }
+
+    bool _isvalidaEmail(String val) {
+      if (val.isEmpty) {
+        return false;
+      } else if (!EmailValidator.validate(val, true)) {
+        return false;
+      }
+      return true;
+    }
+
+    String? validatePassword(String val) {
+      if (val.isEmpty) {
+        return "Password can not be empty";
+      } else if (val.length < 8) {
+        return "Password should be atleast 8 charecters long";
+      }
+      return '';
+    }
+
+    bool _isvalidPassword(String val) {
+      if (val.isEmpty) {
+        return false;
+      } else if (val.length < 8) {
+        return false;
+      }
+      return true;
+    }
+
     return Scaffold(
       //resizeToAvoidBottomInset: false,
       backgroundColor: const Color.fromARGB(255, 45, 60, 68),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 120,
-                ),
-                const Icon(
-                  Icons.military_tech_outlined,
-                  size: 175,
-                  color: Colors.deepPurpleAccent,
-                ),
-                const SizedBox(height: 80),
-                //welcome text
-                Text(
-                  'Welcome to camp!',
-                  style: GoogleFonts.kanit(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.purple.shade300,
+            child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 120,
                   ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Time to get back!',
-                  style: GoogleFonts.kanit(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.purple.shade400,
+                  const Icon(
+                    Icons.military_tech_outlined,
+                    size: 175,
+                    color: Colors.deepPurpleAccent,
                   ),
-                ),
-                const SizedBox(height: 50),
-
-                // email
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade200,
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
+                  const SizedBox(height: 80),
+                  //welcome text
+                  Text(
+                    'Welcome to camp!',
+                    style: GoogleFonts.kanit(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple.shade300,
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: TextField(
-                        controller: _emailId,
-                        decoration: const InputDecoration(
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Time to get back!',
+                    style: GoogleFonts.kanit(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple.shade400,
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+
+                  // email
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _emailId,
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.email_outlined),
                             border: InputBorder.none,
-                            hintText: 'Email@example.com'),
+                            hintText: 'Email@example.com',
+                            labelText: 'Email ID',
+                          ),
+                          validator: (value) {
+                            if (_isvalidaEmail(value!) == false) {
+                              return validateEmail(value);
+                            } else {
+                              return null;
+                            }
+                          },
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 10),
+                  const SizedBox(height: 10),
 
-                // password
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade200,
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: TextField(
-                        controller: _password,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Enter Password'),
+                  // password
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: TextFormField(
+                          controller: _password,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              prefixIcon: Icon(Icons.lock_open),
+                              hintText: 'Enter Password',
+                              labelText: 'Password'),
+                          validator: (value) {
+                            if (_isvalidPassword(value!) == false) {
+                              return validatePassword(value);
+                            } else {
+                              return null;
+                            }
+                          },
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 10),
-                
-                //Forgot Password button
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return const ForgotPassword();
-                              },
+                  const SizedBox(height: 10),
+
+                  //Forgot Password button
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return const ForgotPassword();
+                                },
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            'Forgot Password? Aiyaa',
+                            style: TextStyle(
+                              color: Colors.amber,
                             ),
-                          );
-                        },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+
+                  // sign in
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        if (_formKey.currentState!.validate()) {
+                          isSignedIn();
+                        }
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(25),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.deepPurple.shade400,
+                              Colors.deepPurple.shade700,
+                            ],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          ),
+                          //color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 35),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'No account?',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.indigo.shade300,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        onTap: widget.showRegisterPage,
                         child: const Text(
-                          'Forgot Password? Aiyaa',
+                          'Create one here!',
                           style: TextStyle(
-                            color: Colors.amber,
+                            color: Colors.tealAccent,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
                     ],
                   ),
-                ),
-                const SizedBox(height: 10),
-
-                // sign in
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: GestureDetector(
-                    onTap: isSignedIn,
-                    child: Container(
-                      padding: const EdgeInsets.all(25),
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.deepPurple.shade400,
-                            Colors.deepPurple.shade700,
-                          ],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ),
-                        //color: Colors.deepPurple,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: const Center(
-                        child: Text(
-                          'Sign In',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 35),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'No account?',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.indigo.shade300,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    GestureDetector(
-                      onTap: widget.showRegisterPage,
-                      child: const Text(
-                        'Create one here!',
-                        style: TextStyle(
-                          color: Colors.tealAccent,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
