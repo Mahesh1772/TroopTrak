378e56b1cf533ccea67434fad3c0a8fc6030ed96
Aak242 <110832731+Aak242@users.noreply.github.com>
2023-06-09 10:19:53 +0800

    Created conduct_details_screen

---
+++ prototype_1/lib/screens/conduct_tracker_screen/conduct_details_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConductDetailsScreen extends StatefulWidget {
+  const ConductDetailsScreen({super.key});
+
+  @override
+  State<ConductDetailsScreen> createState() => _ConductDetailsScreenState();
+}
+
+class _ConductDetailsScreenState extends State<ConductDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
