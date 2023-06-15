5aecbbc22dfcd23e29c0b89adb8cb82ae9f155ed
Aak242 <110832731+Aak242@users.noreply.github.com>
2023-06-15 23:45:31 +0800

    Got data table mostly functional on the guard duty tracker screen with sorting capabilities, design still basic but working

--- prototype_1/lib/screens/guard_duty_tracker_screen.dart/util/duty_personnel_data_source.dart
+++ prototype_1/lib/screens/guard_duty_tracker_screen.dart/util/duty_personnel_data_source.dart
@@ -1,5 +1,7 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:prototype_1/screens/guard_duty_tracker_screen.dart/guard_duty_tracker_screen.dart';
 import 'package:prototype_1/util/text_styles/text_style.dart';
+import 'package:recase/recase.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +9,11 @@ class DutyPersonnelDataSource extends DataGridSource {
   DutyPersonnelDataSource({required List<DutyPersonnel> dutyPersonnel}) {
     _dutyPersonnel = dutyPersonnel
         .map<DataGridRow>((e) => DataGridRow(cells: [
+              DataGridCell<String>(
+                columnName: 'rank',
+                value: e.rank,
+              ),
               DataGridCell<String>(columnName: 'name', value: e.name),
-              DataGridCell<String>(columnName: 'image', value: e.image),
-              DataGridCell<String>(columnName: 'rank', value: e.rank),
               DataGridCell<int>(columnName: 'points', value: e.points),
             ]))
         .toList();
@@ -24,18 +28,45 @@ class DutyPersonnelDataSource extends DataGridSource {
   DataGridRowAdapter? buildRow(DataGridRow row) {
     return DataGridRowAdapter(
         cells: row.getCells().map<Widget>((dataGridCell) {
+      if (dataGridCell.columnName == 'rank') {
+        return Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(16.0.sp),
+          child: Center(
+            child: Image.asset(
+              "lib/assets/army-ranks/${dataGridCell.value.toString().toLowerCase()}.png",
+              width: 35.w,
+              color: rankColorPicker(dataGridCell.value.toString())
+                  ? Colors.white
+                  : null,
+            ),
+          ),
+        );
+      }
+
       return Container(
-        alignment: (dataGridCell.columnName == 'name' ||
-                dataGridCell.columnName == 'points')
-            ? Alignment.centerRight
-            : Alignment.centerLeft,
-        padding: const EdgeInsets.all(16.0),
-        child: StyledText(
-          dataGridCell.value.toString(),
-          20,
-          fontWeight: FontWeight.w600,
+        padding: EdgeInsets.all(16.0.sp),
+        child: Center(
+          child: StyledText(
+            dataGridCell.value.toString().titleCase,
+            16.sp,
+            fontWeight: FontWeight.w400,
+          ),
         ),
       );
     }).toList());
   }
+}
+
+rankColorPicker(String rank) {
+  return (rank == 'REC' ||
+      rank == 'PTE' ||
+      rank == 'LCP' ||
+      rank == 'CPL' ||
+      rank == 'CFC' ||
+      rank == '3SG' ||
+      rank == '2SG' ||
+      rank == '1SG' ||
+      rank == 'SSG' ||
+      rank == 'MSG');
 }
