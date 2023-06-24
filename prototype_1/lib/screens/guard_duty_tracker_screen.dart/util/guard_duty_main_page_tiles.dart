a660d9bce6a795df46dd90f62aa64e455500a488
Aak242 <110832731+Aak242@users.noreply.github.com>
2023-06-24 21:55:52 +0800

    Update guard_duty_main_page_tiles.dart

--- prototype_1/lib/screens/guard_duty_tracker_screen.dart/util/guard_duty_main_page_tiles.dart
+++ prototype_1/lib/screens/guard_duty_tracker_screen.dart/util/guard_duty_main_page_tiles.dart
@@ -47,8 +47,8 @@ class GuardDutyTile extends StatelessWidget {
               Padding(
                 padding: EdgeInsets.only(right: 30.0.w),
                 child: Container(
-                  height: 75.h,
-                  width: 75.w,
+                  height: 80.h,
+                  width: 80.w,
                   decoration: BoxDecoration(
                     color: const Color.fromARGB(255, 53, 14, 145),
                     borderRadius: BorderRadius.circular(10.r),
