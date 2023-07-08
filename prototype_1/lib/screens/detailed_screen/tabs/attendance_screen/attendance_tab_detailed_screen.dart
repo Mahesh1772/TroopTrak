a582a095958a98396e33f7e461da456f933874ba
Aak242 <110832731+Aak242@users.noreply.github.com>
2023-07-08 10:42:11 +0800

    Added the attendance tab on the soldier detailed screen and got it connected to the database

--- prototype_1/lib/screens/detailed_screen/tabs/attendance_screen/attendance_tab_detailed_screen.dart
+++ prototype_1/lib/screens/detailed_screen/tabs/attendance_screen/attendance_tab_detailed_screen.dart
@@ -1,4 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:prototype_1/screens/detailed_screen/util/book_in_out_tile.dart.dart';
+
+late Stream<QuerySnapshot> documentStream;
+List<Map<String, dynamic>> userBookInStatus = [];
 
 class AttendanceTab extends StatefulWidget {
   const AttendanceTab({
@@ -14,7 +21,81 @@ class AttendanceTab extends StatefulWidget {
 
 class _AttendanceTabState extends State<AttendanceTab> {
   @override
+  void initState() {
+    documentStream = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(widget.docID)
+        .collection('Attendance')
+        .snapshots();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Padding(
+        padding: EdgeInsets.all(30.sp),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: documentStream,
+          builder: (context, snapshot) {
+            var users = snapshot.data?.docs.toList();
+
+            if (snapshot.hasData) {
+              userBookInStatus = [];
+
+              for (var i = 0; i < users!.length; i++) {
+                var data = users[i].data();
+                userBookInStatus.add(data as Map<String, dynamic>);
+                userBookInStatus[i]
+                    .addEntries({'ID': users[i].reference.id}.entries);
+              }
+            }
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.outbond,
+                      color: Colors.white,
+                      size: 30.sp,
+                    ),
+                    SizedBox(
+                      width: 20.w,
+                    ),
+                    Text(
+                      "Book In / Book Out",
+                      maxLines: 2,
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 400.h,
+                  child: ListView.builder(
+                    itemCount: userBookInStatus.length,
+                    padding: EdgeInsets.all(12.sp),
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (context, index) {
+                      return BookInOutTile(
+                        timeStamp: userBookInStatus[index]['date&time'],
+                        isInsideCamp: userBookInStatus[index]['isInsideCamp'],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
---
+++ prototype_1/lib/screens/detailed_screen/util/book_in_out_tile.dart.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class BookInOutTile extends StatefulWidget {
+  const BookInOutTile({
+    super.key,
+    required this.timeStamp,
+    required this.isInsideCamp,
+    //required this.docID,
+  });
+
+  final String timeStamp;
+  final bool isInsideCamp;
+
+  //final String docID;
+
+  @override
+  State<BookInOutTile> createState() => _BookInOutTileState();
+}
+
+class _BookInOutTileState extends State<BookInOutTile> {
+  @override
+  Widget build(BuildContext context) {
+    late IconData? tileIcon;
+    late Color tileColor;
+    tileIcon = setTileIcon(widget.isInsideCamp);
+    tileColor = setTileColor(widget.isInsideCamp);
+
+    return Padding(
+      padding: EdgeInsets.only(bottom: 8.0.h),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (context) {},
+              icon: Icons.delete_forever_rounded,
+              backgroundColor: Colors.red,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(12.r),
+                bottomRight: Radius.circular(12.r),
+              ),
+            ),
+          ],
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            color: tileColor,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12.r),
+                bottomLeft: Radius.circular(12.r)),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(16.0.sp),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(
+                  tileIcon,
+                  color: Colors.white,
+                  size: 30.sp,
+                ),
+                SizedBox(width: 20.w),
+                SizedBox(
+                  width: 100.w,
+                  child: AutoSizeText(
+                    widget.isInsideCamp ? "BOOK IN" : "BOOK OUT",
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.sp,
+                        color: Colors.white),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                SizedBox(
+                  width: 180.w,
+                  child: AutoSizeText(
+                    widget.timeStamp,
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14.sp,
+                        color: Colors.white),
+                    maxLines: 1,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Color setTileColor(bool isInsideCamp) {
+    if (isInsideCamp) {
+      return Colors.green.shade600;
+    } else {
+      return Colors.red;
+    }
+  }
+
+  IconData? setTileIcon(bool type) {
+    if (type) {
+      return Icons.work_history;
+    } else {
+      return Icons.home;
+    }
+  }
+}
